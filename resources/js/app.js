da55b3a730ef777ac7c4726812ba9f37ffc4ac2a
Frankaz <frankazx@gmail.com>
2025-02-19 15:13:36 +0000

    Add FontAwesome

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,13 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {fas} from "@fortawesome/free-solid-svg-icons";
 
+library.add(fas)
+
+const app = createApp({}).component('font-awesome-icon', FontAwesomeIcon);
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
