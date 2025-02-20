7591dae61ea7a2e8b5b25b1d69ddbf052283f7bc
Frankaz <frankazx@gmail.com>
2025-02-20 13:47:20 +0000

    Resolve Some Dependencies

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,13 +6,17 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
-import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
-import {library} from "@fortawesome/fontawesome-svg-core";
-import {fas} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { createBootstrap } from 'bootstrap-vue-next';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
 
-library.add(fas)
+library.add(fas);
 
 const app = createApp({}).component('font-awesome-icon', FontAwesomeIcon);
+app.use(createBootstrap());
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import Components from 'unplugin-vue-components/vite';
-import {BootstrapVueNextResolver} from 'bootstrap-vue-next';
 
 export default defineConfig({
     plugins: [
@@ -17,9 +15,6 @@ export default defineConfig({
                     includeAbsolute: false,
                 },
             },
-        }),
-        Components({
-            resolvers: [BootstrapVueNextResolver()],
         }),
     ],
 });
