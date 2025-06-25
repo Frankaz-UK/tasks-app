eea664f43466dee03f60ac1473c44c0aa72beae4
Frankaz <frankazx@gmail.com>
2025-06-25 21:17:38 +0100

    Add Users List API Protect All Routes Apart From Index With Auth

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use DB;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Throwable;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function index(Request $request): JsonResponse
+    {
+        try {
+            $users = User::query()
+                ->select(
+                    DB::raw("CONCAT(forename,' ',surname) as fullname"),
+                    'id'
+                )
+                ->without('roles')
+                ->orderBy('id')
+                ->get();
+        } catch (Throwable $exception) {
+            return response()->json([
+                'message' => $exception->getMessage(),
+            ],
+            Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+
+        return response()->json([
+            'results' => $users,
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TaskController;
@@ -16,30 +17,38 @@ Route::get('/', function () {
     ]);
 })->name('home');
 
-Route::prefix('/dashboard')->name('dashboard.')->group(function () {
-    Route::get('/', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('index');
-
-    Route::prefix('/api')->name('api.')->group(function () {
-        Route::get('/', [\App\Http\Controllers\Api\DashboardController::class, 'index'])->name('index');
-    });
-});
-
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+    Route::prefix('/dashboard')->name('dashboard.')->group(function () {
+        Route::get('/', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('index');
 
-Route::prefix('/tasks')->name('tasks.')->group(function () {
-    Route::get('/', [TaskController::class, 'index'])->name('index');
+        Route::prefix('/api')->name('api.')->group(function () {
+            Route::get('/', [\App\Http\Controllers\Api\DashboardController::class, 'index'])->name('index');
+        });
+    });
 
-    Route::prefix('/api')->name('api.')->group(function () {
-        Route::get('/', [\App\Http\Controllers\Api\TaskController::class, 'index'])->name('index');
-        Route::post('/', [\App\Http\Controllers\Api\TaskController::class, 'store'])->name('store');
-        Route::patch('/{task}', [\App\Http\Controllers\Api\TaskController::class, 'update'])->name('update');
-        Route::patch('/{task}/status', [\App\Http\Controllers\Api\TaskController::class, 'changeStatus'])->name('status');
-        Route::delete('/{task}', [\App\Http\Controllers\Api\TaskController::class, 'destroy'])->name('destroy');
+    Route::middleware('auth')->group(function () {
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
+
+    Route::prefix('/tasks')->name('tasks.')->group(function () {
+        Route::get('/', [TaskController::class, 'index'])->name('index');
+
+        Route::prefix('/api')->name('api.')->group(function () {
+            Route::get('/', [\App\Http\Controllers\Api\TaskController::class, 'index'])->name('index');
+            Route::post('/', [\App\Http\Controllers\Api\TaskController::class, 'store'])->name('store');
+            Route::patch('/{task}', [\App\Http\Controllers\Api\TaskController::class, 'update'])->name('update');
+            Route::patch('/{task}/status', [\App\Http\Controllers\Api\TaskController::class, 'changeStatus'])->name('status');
+            Route::delete('/{task}', [\App\Http\Controllers\Api\TaskController::class, 'destroy'])->name('destroy');
+        });
+    });
+
+    Route::prefix('/users')->name('users.')->group(function () {
+
+        Route::prefix('api')->name('api.')->group(function () {
+            Route::get('/', [UserController::class, 'index'])->name('index');
+        });
     });
 });
-
 require __DIR__.'/auth.php';
